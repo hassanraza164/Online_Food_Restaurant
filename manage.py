a3bb38718a375be7b693a6532933cf860cd956cb
hassanraza164 <hassain18071999@gmail.com>
2022-05-13 23:01:34 +0500

    changing in manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Food_Delivery.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Food_Delivery.settings ')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()   
